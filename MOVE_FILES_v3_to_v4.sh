07bd22d4bbcee42ee9ca3b7920110972bacf637b
tieje <toj320@gmail.com>
2021-09-25 11:36:25 -0400

    created templates folder and move template there

--- MOVE_FILES_v3_to_v4.sh
+++ MOVE_FILES_v3_to_v4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp -R web_flask/static/ /web_dynamic/static/
 cp web_flask/__init__.py web_dynamic/__init__.py
-cp web_flask/templates/100-hbnb.html web_dynamic/0-hbnb.html
+cp web_flask/templates/100-hbnb.html web_dynamic/templates/0-hbnb.html
 cp web_flask/100-hbnb.py web_dynamic/0-hbnb.py
