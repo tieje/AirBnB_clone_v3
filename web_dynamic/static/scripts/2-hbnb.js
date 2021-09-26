cc6c2de9bc93d1b37d03c9f5c537f74cd029d76d
Carson Stearn <cstearn@gmail.com>
2021-09-26 17:58:38 +0000

    task 3, updated older files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,4 +8,11 @@ $(document).ready(function () {
     }
     $('.amenities H4').text(Object.values(amenities).join(', '));
   });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (response) {
+      if (response.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    });
 });
